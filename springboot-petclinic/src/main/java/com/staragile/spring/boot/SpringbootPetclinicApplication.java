7cbae26cf9ebed0f5d132071c3437bc4c48fb8bc
Suresh-kumar55 <sureshsuryakumar55@gmail.com>
2024-02-12 14:09:44 +0530

    first commit

---
+++ springboot-petclinic/src/main/java/com/staragile/spring/boot/SpringbootPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.staragile.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPetclinicApplication.class, args);
+	}
+
+}
